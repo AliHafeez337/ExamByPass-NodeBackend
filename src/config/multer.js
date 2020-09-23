8048c74058d3b94c0ad9284d817947c2355d45ec
daud99 <51257049+daud99@users.noreply.github.com>
2020-09-23 15:50:32 +0500

    working

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,7 +6,7 @@ let storage = multer.diskStorage({
     cb(null, path.join(__dirname, '../', 'uploads'))
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
+    cb(null, file.originalname)
   }
 })
 
--- src/routes/fileUpload.js
+++ src/routes/fileUpload.js
@@ -9,66 +9,93 @@ import upload from '../config/multer'
 // Variables
 const router = express.Router();
 
-router.post(
-  '/eteFile',
-  (req, res) => {
-    upload(req, res, async function (err) {
+router.post('/eteFile', (req,res)=> {
+  var FormData = require('form-data');
+  var data = new FormData();
+  data.append('file', fs.createReadStream('BCCPP.ete'));
+
+  var config = {
+    method: 'post',
+    url: 'http://25.34.216.242:3000/upload',
+    headers: { 
+      ...data.getHeaders()
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+})
+
+
+// router.post(
+//   '/eteFile',
+//   (req, res) => {
+//     upload(req, res, async function (err) {
       
-      let newFile = await fs.createReadStream(req.file.path);
-      const form_data = new FormData();
-      form_data.append('file', newFile);
+//       // let newFile = await fs.createReadStream(req.file.path);
+//       console.log(file);
+//       const form_data = new FormData();
+//       form_data.append('file', newFile);
 
-      axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-			axios.post(`http://25.34.216.242:3000/upload`, form_data)
-      .then(res2 => {
-        console.log(res2.data);
-        res.status(200).send({
-          msg: "File uploaded."
-        })
-      })
-      .catch(err2 => {
-        console.log(err2)
-        res.status(400).send({
-          errmsg: err2
-        })
-      })
+//       axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+// 			axios.post(`http://25.34.216.242:3000/upload`, form_data)
+//       .then(res2 => {
+//         console.log(res2.data);
+//         res.status(200).send({
+//           msg: "File uploaded."
+//         })
+//       })
+//       .catch(err2 => {
+//         console.log(err2)
+//         res.status(400).send({
+//           errmsg: err2
+//         })
+//       })
 
-      // try {
-      //   var res2 = await axios({
-      //     method: "post",
-      //     url: 'http://25.34.216.242:3000/upload',
-      //     headers: {
-      //         // "Authorization": "Bearer " + access_token,
-      //         "Content-Type": "multipart/form-data"
-      //     },
-      //     data: form_data
-      //   });
-      //   console.log(res2.data)
+//       // try {
+//       //   var res2 = await axios({
+//       //     method: "post",
+//       //     url: 'http://25.34.216.242:3000/upload',
+//       //     headers: {
+//       //         // "Authorization": "Bearer " + access_token,
+//       //         "Content-Type": "multipart/form-data"
+//       //     },
+//       //     data: form_data
+//       //   });
+//       //   console.log(res2.data)
   
-      //   if (res2.data){
-      //     if (res2.data.upload === 'done'){
-      //       res.status(200).send({
-      //         msg: "File uploaded."
-      //       })
-      //     } else if (res2.data.errmsg){
-      //       res.status(400).send({
-      //         errmsg: res2.data.errmsg
-      //       })
-      //     }
-      //   } else {
-      //     res.status(400).send({
-      //       errmsg: "Unable to upload file."
-      //     })
-      //   }
-      // } catch(err2) {
-      //   console.log(err2)
-      //   res.status(400).send({
-      //     errmsg: err2
-      //   })
-      // }
+//       //   if (res2.data){
+//       //     if (res2.data.upload === 'done'){
+//       //       res.status(200).send({
+//       //         msg: "File uploaded."
+//       //       })
+//       //     } else if (res2.data.errmsg){
+//       //       res.status(400).send({
+//       //         errmsg: res2.data.errmsg
+//       //       })
+//       //     }
+//       //   } else {
+//       //     res.status(400).send({
+//       //       errmsg: "Unable to upload file."
+//       //     })
+//       //   }
+//       // } catch(err2) {
+//       //   console.log(err2)
+//       //   res.status(400).send({
+//       //     errmsg: err2
+//       //   })
+//       // }
 
-    })
-  }
-)
+//     })
+//   }
+// )
 
 export { router as default }
